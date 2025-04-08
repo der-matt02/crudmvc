58489e6be7072328e18a9a40920d5b56cb183d20
der-matt02 <dermatt02@gmail.com>
2025-04-08 18:11:49 -0500

    Implemetacion de controller y cambios de nombres de variables en db y user model

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -6,7 +6,7 @@ from ..db.database import Base
 class UserModel(Base):
     __tablename__ = "users"
 
-    id = Column(CHAR(36), primary_key=True, default=lambda: str(uuid.uuid4()))
-    name = Column(String(100), nullable=False)
-    email = Column(String(100), unique=True, nullable=False)
-    address = Column(String(255), nullable=True)
+    user_id = Column(CHAR(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    user_name = Column(String(100), nullable=False)
+    user_email = Column(String(100), unique=True, nullable=False)
+    user_address = Column(String(255), nullable=True)
