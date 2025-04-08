59e293d3a67cc51d977184566017fabe193ccdc4
der-matt02 <dermatt02@gmail.com>
2025-04-08 17:18:48 -0500

    creacion de schema

---
+++ app/schemas/user_schema.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+class CustomerSchema(BaseModel):
+    name: Optional[str] = None
+    email: Optional[EmailStr] = None
+    address: Optional[str] = None
+
+    class Config:
+        orm_mode = True
